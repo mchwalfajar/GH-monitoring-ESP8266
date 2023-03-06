ff4264ba400c70ddf2beab86bacfd5a30f9d5007
Adam Shaffa <111060241+adamshaffa@users.noreply.github.com>
2023-03-06 11:26:50 +0700

    simplify the code
    This code has simplified variable names, and the values are directly converted into JSON-formatted string data. It then sends the data to the Antares platform using the send() function.
    
    Note that I also including the projectName and deviceName variables as arguments to the send() function, and we're no longer using the add() function. This is because I already formatting the data as a JSON string, so we don't need to use the add() function to format the data for us. Finally, I also moved the delay statement to the end of the loop() function, so that the code first collects all sensor readings and sends the data, before it waits for the next iteration.

--- GH-monitor-ESP8266.ino
+++ GH-monitor-ESP8266.ino
@@ -4,46 +4,40 @@
 #include <Wire.h>
 #include <BH1750.h>
 
-/* credential */
+/* Credentials */
 #define ACCESSKEY "181b1730efb9f453:1860de96ede6acb0"
 #define WIFISSID "GH-monitor"
 #define PASSWORD "sakuraprada"
-#define projectName "greenhouse-nodemcu"
-#define deviceName "GH-monitor"
+#define PROJECTNAME "greenhouse-nodemcu"
+#define DEVICENAME "GH-monitor"
 
 AntaresESP8266HTTP antares(ACCESSKEY);
 
 #define DHTPIN 14          // 3=RX, 4=D2(SDA), 5=D1(SCL), 12=D6, 13=D7, 14=D5
 #define DHTTYPE DHT22     
-#define soilPin A0
-int soilValue;
+#define SOILPIN A0
 
 BH1750 lightMeter;
 DHT dht(DHTPIN, DHTTYPE);
-float t,h,s,l;
 
 void setup() {
   Serial.begin(115200);
   antares.setDebug(true);
-  antares.wifiConnection(WIFISSID,PASSWORD);
+  antares.wifiConnection(WIFISSID, PASSWORD);
   Wire.begin();
   lightMeter.begin();
   dht.begin();
 }
 
 void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float s = analogRead(SOILPIN)/60.0;
+  float l = lightMeter.readLightLevel();
+
+  String data = String("{\"H\":") + h + ",\"T\":" + t + ",\"S\":" + s + ",\"L\":" + l + "}";
+
+  antares.send(PROJECTNAME, DEVICENAME, data);
+
   delay(30000);          //600.000 = 10 menit
-
-  h = dht.readHumidity();
-  t = dht.readTemperature();
-  soilValue = analogRead(soilPin);
-  s = soilValue/60;
-  l = lightMeter.readLightLevel();
-
-  antares.add("H", h);
-  antares.add("T", t);
-  antares.add("S", s);
-  antares.add("L", l);
-
-  antares.send(projectName, deviceName);
 }
